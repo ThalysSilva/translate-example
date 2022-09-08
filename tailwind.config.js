2f99456c7ed8b3a4ef2591e81e042665bf6f56dd
Thalys Silva <thalysfarias11@gmail.com>
2022-09-08 15:22:13 -0300

    feat: adding and configure tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        heightModalAnimation: {
+          "0%": {
+            maxHeight: "0px",
+
+            opacity: 0,
+          },
+
+          "100%": {
+            maxHeight: "476px",
+
+            opacity: 1,
+          },
+        },
+
+        fadeInAnimation: {
+          "0%": { opacity: 0 },
+
+          "100%": { opacity: 1 },
+        },
+      },
+
+      animation: {
+        heightModalAnimation: "heightModalAnimation 500ms ease-in-out",
+        fadeInAnimation:" fadeInAnimation 200ms ease-in-out"
+      },
+    },
+  },
+  plugins: [],
+}
