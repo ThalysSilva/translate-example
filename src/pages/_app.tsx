fabd0d4a6e5baf10f898b19b073100c05864ba74
Thalys Silva <thalysfarias11@gmail.com>
2022-09-08 23:12:44 -0300

    feat: add reactQuery to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { ReactElement, ReactNode } from "react";
+import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>{"Sleepie"}</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
